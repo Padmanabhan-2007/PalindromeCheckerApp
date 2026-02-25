f648493a4f8b6256b67c900c6cb92af8b37ecc48
Padmanabhan-2007 <NULL>
2026-02-25 15:33:00 +0530

    UC5: Stack-Based Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,27 @@
-public class UseCase4PalindromeCheckerApp {
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String original = "racecar";
-        char[] chars = original.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
+        // Original string to check
+        String original = "deed";
 
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        // Create a stack of Characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters of the string into the stack
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
         }
-        if (isPalindrome) {
+
+        // Build the reversed string by popping from stack
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        // Compare original and reversed strings
+        if (original.equals(reversed)) {
             System.out.println("The string \"" + original + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
